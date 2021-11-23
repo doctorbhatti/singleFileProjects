7d2673ac519a70232bdeabf35f273a2d6b570885
doctorbhatti <dochassan12@outlook.com>
2021-11-23 18:11:58 +0500

    formatted code

--- consoleTable.js
+++ consoleTable.js
@@ -1,27 +1,27 @@
 const techGiants = [
   {
     name: "Apple",
-    sharePrice:2244
+    sharePrice: 2244
   },
   {
     name: "AliBaba",
-    sharePrice:5608
+    sharePrice: 5608
   },
   {
     name: "Facebook",
-    sharePrice:2454
+    sharePrice: 2454
   },
   {
     name: "Amazaon",
-    sharePrice:5608
+    sharePrice: 5608
   },
   {
     name: "Twitter",
-    sharePrice:8244
+    sharePrice: 8244
   },
   {
     name: "Microsoft",
-    sharePrice:5690
+    sharePrice: 5690
   }
   ]
-  console.table(techGiants)
+console.table(techGiants)
